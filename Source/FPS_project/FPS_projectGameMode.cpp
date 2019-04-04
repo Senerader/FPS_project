9052fed5924b8ca4baf0e7f4403472a5e3861104
Senerader <markas050@gmail.com>
2019-04-04 19:04:59 +0300

    Architecture of project files refactoring. Fixing camera strafe glitch, fixing hard coded filepaths. Fixing the gun firing bug (Changing collision preset on collision boxes to trigger).

--- Source/FPS_project/FPS_projectGameMode.cpp
+++ Source/FPS_project/FPS_projectGameMode.cpp
@@ -9,7 +9,7 @@ AFPS_projectGameMode::AFPS_projectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
